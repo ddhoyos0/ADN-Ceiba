7445f3f6c6fe8d8e0c7d4704abd996ae4531eed1
Deymer de Jesús Hoyos Pérez <48861824+ddhoyos0@users.noreply.github.com>
2020-04-12 14:46:24 -0500

    adaptador empleado modificado

--- src/test/java/com/djhoyos/citasweb/infraestructura/adaptador_jpa/repositorio/adaptador/EmpleadoRepositorioAdaptadorTest.java
+++ src/test/java/com/djhoyos/citasweb/infraestructura/adaptador_jpa/repositorio/adaptador/EmpleadoRepositorioAdaptadorTest.java
@@ -1,8 +1,11 @@
 package com.djhoyos.citasweb.infraestructura.adaptador_jpa.repositorio.adaptador;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.djhoyos.citasweb.databuider.TestDataBuilder;
 import com.djhoyos.citasweb.dominio.modelo.Empleado;
@@ -33,13 +37,27 @@ class EmpleadoRepositorioAdaptadorTest {
 		empleado = new Empleado();
 	}
 
-	
+	@Test
+	@Transactional
+	@Sql(scripts = "/datos.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	public void crearEmpleado() {
+		Empleado empleado = datos.buildEmpleado();
+		adaptador.crear(empleado);
+		assertTrue(adaptador.listar().size() > 0);
+
+	}
 	@Test
 	public void listarEmpleado() {
 		 List<Empleado> lista = adaptador.listar();
 		 assertNotNull(lista);
 
 	}	
-		
+	
+	
+	@Test
+	public void eliminaarEmpleado() {
+		adaptador.eliminar(1);
+		assertTrue(adaptador.listar().size() == 0);
+	}
 
 }
