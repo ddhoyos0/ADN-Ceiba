9f225c30fa447ef0ff63d952b2141abb271c989a
Deymer de Jesús Hoyos Pérez <48861824+ddhoyos0@users.noreply.github.com>
2020-04-12 15:06:13 -0500

    agregando test

--- src/test/java/com/djhoyos/citasweb/infraestructura/adaptador_jpa/repositorio/adaptador/IdentificacionRepositorioAdaptadorTest.java
+++ src/test/java/com/djhoyos/citasweb/infraestructura/adaptador_jpa/repositorio/adaptador/IdentificacionRepositorioAdaptadorTest.java
@@ -1,8 +1,11 @@
 package com.djhoyos.citasweb.infraestructura.adaptador_jpa.repositorio.adaptador;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.djhoyos.citasweb.databuider.TestDataBuilder;
 import com.djhoyos.citasweb.dominio.modelo.Identificacion;
@@ -31,6 +35,16 @@ class IdentificacionRepositorioAdaptadorTest {
 	public void inicio() {
 		datos = new TestDataBuilder();
 		identificacion = new Identificacion();
+	}
+
+	@Test
+	@Transactional
+	@Sql(scripts = "/datos.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	public void crearEmpleado() {
+		Identificacion identificacion = datos.buildDocumento();
+		adaptador.crear(identificacion);
+		assertTrue(adaptador.listar().size() > 0);
+
 	}
 	
 	@Test
--- src/test/java/com/djhoyos/citasweb/infraestructura/adaptador_jpa/repositorio/adaptador/PersonaRepositorioAdaptadorTest.java
+++ src/test/java/com/djhoyos/citasweb/infraestructura/adaptador_jpa/repositorio/adaptador/PersonaRepositorioAdaptadorTest.java
@@ -1,8 +1,11 @@
 package com.djhoyos.citasweb.infraestructura.adaptador_jpa.repositorio.adaptador;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,33 +14,58 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.djhoyos.citasweb.databuider.TestDataBuilder;
 import com.djhoyos.citasweb.dominio.modelo.Persona;
 
 @DataJpaTest
-@Import({ PersonaRepositorioAdaptador.class})
+@Import({ PersonaRepositorioAdaptador.class })
 @EnableAutoConfiguration
 @TestPropertySource(locations = "/test.properties")
 class PersonaRepositorioAdaptadorTest {
-	
+
 	@Autowired
 	private PersonaRepositorioAdaptador adaptador;
 	public TestDataBuilder datos;
-	public Persona persona;	
-	
+	public Persona persona;
+
 	@BeforeEach
 	public void inicio() {
 		datos = new TestDataBuilder();
 		persona = new Persona();
 	}
-	
+
 	@Test
-	public void listarPersonas() {
-		 List<Persona> lista = adaptador.listar();
-		 assertNotNull(lista);
+	@Transactional
+	@Sql(scripts = "/datos.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	public void crearPersona() {
+		Persona persona = datos.buildPersona();
+		adaptador.crear(persona);
+		assertTrue(adaptador.listar().size() > 0);
 
 	}
 
-	
+	@Test
+	@Transactional
+	@Sql(scripts = "/datos.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	public void actualizarPersona() {
+		Persona persona = datos.buildPersona();
+		adaptador.actualizar(persona);
+		assertTrue(adaptador.listar().size() > 0);
+	}
+
+	@Test
+	public void listarPersonas() {
+		List<Persona> lista = adaptador.listar();
+		assertNotNull(lista);
+
+	}
+
+	@Test
+	public void eliminarPersona() {
+		adaptador.eliminar(1);
+		assertTrue(adaptador.listar().size() == 0);
+	}
+
 }
